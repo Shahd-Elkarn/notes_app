c2793dd9b2ebf70687868de902d36cb142d4851a
shahd-Elkarn <shahdelkarn536@gmail.com>
2025-01-18 14:02:46 +0200

    register adapter

--- lib/cubits/add_note_cubit/add_note_cubit.dart
+++ lib/cubits/add_note_cubit/add_note_cubit.dart
@@ -2,9 +2,17 @@
 
 import 'package:bloc/bloc.dart';
 import 'package:flutter/material.dart';
+import 'package:notes_app/models/note_model.dart';
 
 part 'add_note_state.dart';
 
 class AddNoteCubit extends Cubit<AddNoteState> {
   AddNoteCubit() :super(AddNoteInitial());
+
+
+  addNote(NoteModel note){}
+
+
+
+
 }
--- lib/cubits/add_note_cubit/add_note_state.dart
+++ lib/cubits/add_note_cubit/add_note_state.dart
@@ -10,4 +10,8 @@ class AddNoteLoading extends AddNoteState{}
 
 class AddNoteSuccess extends AddNoteState{}
 
-class AddNoteFailure extends AddNoteState{}
+class AddNoteFailure extends AddNoteState{
+  final String errorMessage;
+
+  AddNoteFailure({required this.errorMessage});
+}
